bb0476663fa0e80124c146de47d870bc37f080d0
ChimPanhaDev <panhachim168@gmail.com>
2026-01-01 11:35:09 +0700

    refactor: reorganize note-related files into a dedicated directory structure

--- routes.php
+++ routes.php
@@ -2,8 +2,8 @@
 return [
     '/'            => 'controller/index.php',
     '/about'       => 'controller/about.php',
-    '/notes'       => 'controller/notes.php',
+    '/notes'       => 'controller/notes/notes.php',
     '/contact'     => 'controller/contact.php',
-    '/note'        => 'controller/note.php',
-    '/create-note' => 'controller/createnote.php',
+    '/note'        => 'controller/notes/note.php',
+    '/create-note' => 'controller/notes/createnote.php',
 ];
