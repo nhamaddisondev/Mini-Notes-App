d23efc1d448dbe9e9623f4d88cc478188dcce977
ChimPanhaDev <panhachim168@gmail.com>
2026-02-08 00:52:59 +0700

    refactor: update registration view to reflect account creation instead of sign-in

--- views/registration/create.view.php
+++ views/registration/create.view.php
@@ -39,8 +39,7 @@
                 <div class="sm:mx-auto sm:w-full sm:max-w-sm">
                     <img src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
                         alt="Your Company" class="mx-auto h-10 w-auto" />
-                    <h2 class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">Sign in to your
-                        account</h2>
+                    <h2 class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">Register for a new account</h2>
                 </div>
 
                 <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
@@ -56,10 +55,6 @@
                         <div>
                             <div class="flex items-center justify-between">
                                 <label for="password" class="block text-sm/6 font-medium text-gray-900">Password</label>
-                                <div class="text-sm">
-                                    <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot
-                                        password?</a>
-                                </div>
                             </div>
                             <div class="mt-2">
                                 <input id="password" type="password" name="password" required
@@ -70,8 +65,7 @@
 
                         <div>
                             <button type="submit"
-                                class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign
-                                in</button>
+                                class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Register</button>
                         </div>
                     </form>
                 </div>
