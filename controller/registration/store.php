f647e6a4f83dc234f3c94575b449222d3debc16a
ChimPanhaDev <panhachim168@gmail.com>
2026-02-14 01:21:40 +0700

    feat: hash user passwords during registration and update header to display user email

--- controller/registration/store.php
+++ controller/registration/store.php
@@ -54,7 +54,7 @@ $db->query(
     'INSERT INTO users (email, password) VALUES (:email, :password)',
     [
         'email' => $email,
-        'password' => $password
+        'password' => password_hash($password, PASSWORD_BCRYPT)
     ]
 );
 
