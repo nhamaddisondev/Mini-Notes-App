f73bf89924e5da575a1774a9ab33bcd2784d0be5
ChimPanhaDev <panhachim168@gmail.com>
2026-03-22 10:08:44 +0700

    feat: add error handling for email and password fields in login form

--- views/session/create.view.php
+++ views/session/create.view.php
@@ -49,8 +49,12 @@
                             <label for="email" class="block text-sm/6 font-medium text-gray-900">Email address</label>
                             <div class="mt-2">
                                 <input id="email" type="email" name="email" required autocomplete="email"
+                                    value="<?= htmlspecialchars($_POST['email'] ?? '') ?>"
                                     class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
                             </div>
+                            <?php if (isset($errors['email'])): ?>
+                                <p class="mt-2 text-sm text-red-600"><?= htmlspecialchars($errors['email']) ?></p>
+                            <?php endif; ?>
                         </div>
 
                         <div>
@@ -62,6 +66,9 @@
                                     autocomplete="current-password"
                                     class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
                             </div>
+                            <?php if (isset($errors['password'])): ?>
+                                <p class="mt-2 text-sm text-red-600"><?= htmlspecialchars($errors['password']) ?></p>
+                            <?php endif; ?>
                         </div>
 
                         <div>
