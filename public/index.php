b31a3febdcae5225c91cdd24cd9ff151f9670497
ChimPanhaDev <panhachim168@gmail.com>
2026-04-29 00:30:55 +0700

    Add PHPUnit configuration and initial test setup
    - Introduced .pest.xml and phpunit.xml for PHPUnit configuration.
    - Created a playground script to demonstrate Laravel Collection usage.
    - Added initial feature and unit tests in the tests directory.
    - Established a base TestCase class for consistent test structure.

--- public/index.php
+++ public/index.php
@@ -5,7 +5,6 @@ use Core\ValidationException;
 
 session_start();
 
-@@ -22,7 +23,14 @@
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 $method = $_POST['_method'] ?? $_SERVER['REQUEST_METHOD'];
 
